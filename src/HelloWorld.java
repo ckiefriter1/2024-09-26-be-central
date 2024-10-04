d4050efcf6b0c74b2117fecdedfc8d7909344500
Chuck Kiefriter <ckiefriter@gmail.com>
2024-10-03 21:49:04 -0400

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// chuck wuz here
+		
+		System.out.println("*** Hello World!");
+
+	}
+
+}
